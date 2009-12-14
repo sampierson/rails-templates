96cf416068ce4cf0bdf32c83f361cdb2a4b5e843
Sam Pierson <sam@sampierson.com>
2009-12-13 22:08:31 -0800

    Add rspec and haml support to itdoesnothing.rb

--- itdoesnothing.rb
+++ itdoesnothing.rb
@@ -1,4 +1,5 @@
 run "echo TODO > README"
+
 git :init
 run "find . -type d -empty | while read dir ; do touch $dir/.gitignore ; done"
 file ".gitignore", <<-END
@@ -9,5 +10,15 @@ db/*.sqlite3
 .idea
 !.gitignore
 END
+
 git :add => "."
 git :commit => "-m 'virgin Rails application'"
+
+generate :rspec
+git :add => "."
+git :commit => "-m 'script/generate rspec'"
+
+gem 'haml', :version => '>= 2.2.15'
+run "haml --rails ."
+git :add => "."
+git :commit => "-m 'Added HAML support'"
