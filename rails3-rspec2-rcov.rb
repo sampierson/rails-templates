1ed289f4f2fcc3373b7ef4bfa9a6ac6e4f2fff0f
Sam Pierson <sam@sampierson.com>
2010-10-07 15:01:08 -0400

    add rails3-rspec2-rcov.rb

---
+++ rails3-rspec2-rcov.rb
@@ -0,0 +1,12 @@
+#
+# Rails 3 application with test framework:
+#   RSpec2
+#   rcov
+#
+# Compatible with Ruby 1.8.7
+#   Rails 3 requires Ruby 1.8.7
+#   rcov does not support Ruby 1.9 at this time
+#
+apply 'http://github.com/ombwa/rails-templates/raw/master/_base+git.rb'
+apply 'http://github.com/ombwa/rails-templates/raw/master/_rspec2.rb'
+apply 'http://github.com/ombwa/rails-templates/raw/master/_rcov.rb'
