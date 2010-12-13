7b9121c8ecd6295131bd897d10815ee2de42f7e6
Sam Pierson <sam@sampierson.com>
2010-12-13 13:50:20 -0800

    itdoesnothing now uses compass, html5 boilerplate
    and will install the correct code coverage tool for the correct Ruby version

--- itdoesnothing.rb
+++ itdoesnothing.rb
@@ -4,18 +4,18 @@
 #   rails new <name> --skip-test-unit --skip-prototype --template=https://girhub.com/sampierson/rails-templates/raw/master/itdoesnothing.rb
 #
 
-TEMPLATES_REPOSITORY = 'https://github.com/sampierson/rails-templates/raw/master'
+TEMPLATES_REPOSITORY = ENV['TEMPLATES_REPOSITORY'] || 'https://github.com/sampierson/rails-templates/raw/master'
 
-%w{
-  base+git
-  rspec2
-  cover_me
-  cucumber+capybara
-  haml
-  haml_layout
-  home_controller
-  devise
-  jquery
-}.each do |template|
+TEMPLATES = [
+  'utils',
+  'base+git',
+  'rspec2',
+  (RUBY_VERSION =~ /1.9/ ? 'cover_me' : 'rcov'),  # Test coverage measurement
+  'cucumber+capybara',                            # Integration test framework: cucumber-capybara cucumber+webrat
+  'compass+html5',                                # Templating/layout: haml compass+blueprint compass+html5
+  'home_controller',                              # A simple controller around which we can wrap authentication
+  'devise'                                        # Authentication
+  # 'jquery' - done already if we're using compass+html5
+].each do |template|
   apply "#{TEMPLATES_REPOSITORY}/_#{template}.rb"
 end
