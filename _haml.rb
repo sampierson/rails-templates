55b0edc3b94c1bfe2dde2cd50873991e8afc9ba0
Sam Pierson <sam@sampierson.com>
2010-12-13 11:44:10 -0800

    Coalesce _haml and _haml_layout.

--- _haml.rb
+++ _haml.rb
@@ -6,3 +6,10 @@ end
 git_commit('get sass files from app/stylesheets') do
   append_file 'config/environment.rb', "\nSass::Plugin.options[:template_location] = Rails.root.join('app', 'stylesheets').to_s\n"
 end
+
+git_commit('Haml layout') do
+  git :rm => '--quiet app/views/layouts/application.html.erb'
+  install_template 'app/views/layouts/application.html.haml'
+  install_template 'app/stylesheets/application.sass'
+  append_file '.gitignore', '/public/stylesheets'
+end
--- _haml_layout.rb
+++
@@ -1,6 +0,0 @@
-git_commit('Haml layout') do
-  git :rm => '--quiet app/views/layouts/application.html.erb'
-  install_template 'app/views/layouts/application.html.haml'
-  install_template 'app/stylesheets/application.sass'
-  append_file '.gitignore', '/public/stylesheets'
-end
