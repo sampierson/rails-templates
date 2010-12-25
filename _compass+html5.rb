05d6696c5f9549f52dbf714c34e2b2fb88c9eaf0
Sam Pierson <sam@sampierson.com>
2010-12-25 13:05:52 -0800

    Remove application.html.erb after compass init.
    And git ignore public/stylesheets folder.

--- _compass+html5.rb
+++ _compass+html5.rb
@@ -7,3 +7,11 @@ end
 
 run_and_commit('compass init rails -r html5-boilerplate -u html5-boilerplate --force --sass-dir app/stylesheets --css-dir public/stylesheets',
                :message => 'compass init')
+
+git_commit 'gitignore public/stylesheets' do
+  append_file '.gitignore', "public/stylesheets\n"
+end
+
+git_commit 'Remove layouts/application.html.erb' do
+  git :rm => 'app/views/layouts/application.html.erb'
+end
