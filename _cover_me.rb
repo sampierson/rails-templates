e25134dc2c6abea758e668b65340de14a7f78d26
Sam Pierson <sam@sampierson.com>
2010-12-26 07:50:48 -0800

    Fix cover_me configuration so we don't need to disable it in production

--- _cover_me.rb
+++ _cover_me.rb
@@ -3,34 +3,35 @@ git_commit("bundle cover_me gem") do
   bundle_install  
 end
 
-git_commit "rails generate cover_me:install\n\nand configure cover_me in cover_me.rake" do
-
-  bundle_install
-
+git_commit "rails generate cover_me:install" do
   generate :'cover_me:install'
-
-  insert_into_file('lib/tasks/cover_me.rake', :before => "task :test do\n") do
-    <<-'END'
-CoverMe.config do |config|
-  # where is your project's root:
-  # c.project.root => "Rails.root" (default)
-
-  # what files are you interested in coverage for:
-  # c.file_pattern => /(#{CoverMe.config.project.root}\/app\/.+\.rb|#{CoverMe.config.project.root}\/lib\/.+\.rb)/ix (default)
-
-  # where do you want the HTML generated:
-  config.html_formatter.output_path = File.join(CoverMe.config.project.root, 'coverage')
-
-  # what do you want to happen when it finishes:
-  config.at_exit = Proc.new {
-  #   if CoverMe.config.formatter == CoverMe::HtmlFormatter
-  #     index = File.join(CoverMe.config.html_formatter.output_path, 'index.html')
-  #     if File.exists?(index)
-  #       `open #{index}`
-  #     end
-  #   end
-  }
 end
+
+git_commit "Configure cover_me" do
+
+  insert_into_file('lib/tasks/cover_me.rake', :after => "task :report do\n    require 'cover_me'\n") do
+    <<-'END'
+
+    CoverMe.config do |config|
+      # where is your project's root:
+      # c.project.root => "Rails.root" (default)
+
+      # what files are you interested in coverage for:
+      # c.file_pattern => /(#{CoverMe.config.project.root}\/app\/.+\.rb|#{CoverMe.config.project.root}\/lib\/.+\.rb)/ix (default)
+
+      # where do you want the HTML generated:
+      config.html_formatter.output_path = File.join(CoverMe.config.project.root, 'coverage')
+
+      # what do you want to happen when it finishes:
+      config.at_exit = Proc.new {
+      #   if CoverMe.config.formatter == CoverMe::HtmlFormatter
+      #     index = File.join(CoverMe.config.html_formatter.output_path, 'index.html')
+      #     if File.exists?(index)
+      #       `open #{index}`
+      #     end
+      #   end
+      }
+    end
 
     END
   end
--- _herokuize.rb
+++ _herokuize.rb
@@ -16,11 +16,3 @@ git_commit "Check in CSS files (for Heroku)" do
   gsub_file '.gitignore', "public/stylesheets\n", ''
   run "compass compile"
 end
-
-if File.exist? 'lib/tasks/cover_me.rake'
-  git_commit "Disable cover_me rake tasks in production" do
-    gsub_file    'lib/tasks/cover_me.rake', /^/, '  '
-    prepend_file 'lib/tasks/cover_me.rake', "unless Rails.env == 'production'\n"
-    append_file  'lib/tasks/cover_me.rake', "\nend\n"
-  end
-end
