620efef3377f53b0e86b29449d962cfd106c1185
Sam Pierson <sam@sampierson.com>
2010-12-25 17:53:45 -0800

    Install style for flash

--- _style.rb
+++ _style.rb
@@ -5,6 +5,13 @@
 git_commit "Add some minimal style to shape the layout.\n\n" do
   remove_file 'app/views/layouts/application.html.haml'
   install_template 'app/views/layouts/application.html.haml', :flavor => 'html5'
+
   remove_file 'app/stylesheets/partials/_page.scss'
   install_template 'app/stylesheets/partials/_page.scss', :flavor => 'html5'
+
+  install_template 'app/stylesheets/partials/_flash.scss'
+  append_file 'app/stylesheets/style.scss', "\n@import 'partials/flash';\n"
+  gsub_file 'app/views/layouts/_flashes.html.haml',
+            ':class => key',
+            ':class => "flash #{key}"'
 end
