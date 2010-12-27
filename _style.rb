28b7214434c9bfa7d99d805608feb57be8d72d44
Sam Pierson <sam@sampierson.com>
2010-12-26 21:27:47 -0800

    Add ApplicationHelper#body_classes

--- _style.rb
+++ _style.rb
@@ -6,6 +6,16 @@ git_commit "Add some minimal style to shape the layout.\n\n" do
   remove_file 'app/views/layouts/application.html.haml'
   install_template 'app/views/layouts/application.html.haml', :flavor => 'html5'
 
+  insert_into_file 'app/helpers/application_helper.rb', :before => "\nend" do
+    <<-EOF
+
+  def body_classes
+    controller_name = controller.class.name.underscore.gsub('_controller', '').gsub('/', '_')
+    "\#{controller_name} \#{controller_name}_\#{action_name}"
+  end
+    EOF
+  end
+
   remove_file 'app/stylesheets/partials/_page.scss'
   install_template 'app/stylesheets/partials/_page.scss', :flavor => 'html5'
 
