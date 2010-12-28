734b7dff209a904e22a9acd593f34305ec1a1091
Sam Pierson <sam@sampierson.com>
2010-12-27 22:40:28 -0800

    Disable jasmine rake tasks in production

--- _jasmine.rb
+++ _jasmine.rb
@@ -9,3 +9,9 @@ git_commit "jasmine init" do
   remove_file 'public/javascripts/Song.js'
   remove_file 'spec/javascripts/PlayerSpec.js'
 end
+
+git_commit "disable jasmine rake tasks for production" do
+  gsub_file    'lib/tasks/jasmine.rake', /^/, "  "
+  prepend_file 'lib/tasks/jasmine.rake', "unless Rails.env == 'production'\n"
+  append_file  'lib/tasks/jasmine.rake', "end\n"
+end
